b1c1eda6c765d2cb8eebf6efe058a7c3c1efd308
OnlyM1ss <sojiguy@mail.ru>
2022-12-14 19:08:55 +0300

    added logger

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,15 +1,35 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting http server ")
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLever)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -2,8 +2,9 @@ package apiserver
 
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
+	LogLever string `toml:"log_level"`
 }
 
 func NewConfig() *Config {
-	return &Config{BindAddr: "8080"}
+	return &Config{BindAddr: "8080", LogLever: "debug"}
 }
